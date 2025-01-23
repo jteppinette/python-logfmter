6ddc2461d0d26f9f3a7229ef00fa044c908c8bfd
Joshua Taylor Eppinette <josheppinette@gmail.com>
2025-01-23 13:13:47 -0700

    Add nix support

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python39
+            pkgs.python310
+            pkgs.python311
+            pkgs.python312
+          ];
+          shellHook = "unset PYTHONPATH";
+        };
+      }
+    );
+}
